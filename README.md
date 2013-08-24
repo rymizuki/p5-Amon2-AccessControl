777e826035ef83acf51862b610c6d8f0327d1b69
mizuki_r <ry.mizuki@gmail.com>
2013-08-24 18:38:21 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Amon2::AccessControl - It's new $module
+
+# SYNOPSIS
+
+    use Amon2::AccessControl;
+
+# DESCRIPTION
+
+Amon2::AccessControl is ...
+
+# LICENSE
+
+Copyright (C) mizuki\_r.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+mizuki\_r <ry.mizuki@gmail.com>
---
+++ lib/Amon2/AccessControl.pm
@@ -0,0 +1,39 @@
+package Amon2::AccessControl;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Amon2::AccessControl - It's new $module
+
+=head1 SYNOPSIS
+
+    use Amon2::AccessControl;
+
+=head1 DESCRIPTION
+
+Amon2::AccessControl is ...
+
+=head1 LICENSE
+
+Copyright (C) mizuki_r.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+mizuki_r E<lt>ry.mizuki@gmail.comE<gt>
+
+=cut
+
